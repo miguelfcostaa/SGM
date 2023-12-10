465eb27d9699458dc201b8dc6648a0a1d7cab46d
miguelfcostaa <miguelcosta220@hotmail.com>
2023-12-10 18:26:59 +0000

    photos is almost done!

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -6,10 +6,13 @@ const Biography = () => {
     return (
         <div style={{ background: '#E6E6E6', position: 'absolute', height: '200%', width: '100%' }}>
             <NavBar />
+
             <span className="bioTitle">
                 Biography
             </span>
             <img className='imgBio' height={355} width={240} src='https://cdn.discordapp.com/attachments/707687832239603863/1178771992082653244/image.png?ex=65775bf7&is=6564e6f7&hm=d12904b94d11da7adc667808ad4cc6dabf5b177205a7a1cd9cf7fe8d503be942&' />
+            
+            
             <div className='whiteBoxTitleAbout'>
                 <span style={{ fontSize: '2.2rem', fontFamily:'serif'}}>About</span>
             </div>
@@ -29,6 +32,8 @@ const Biography = () => {
                 <span style={{ fontSize: 22, fontWeight: 'bold'}}> NATIONALITY </span>
                 <span style={{ fontSize: 22, paddingLeft: 290 }}> Portuguese <img height={20} width={28} src='https://www.countryflags.com/wp-content/uploads/portugal-flag-400.png'/> </span>
             </div>
+
+
             <div className='flexCarrerStatsAndTransfer'>
                 <div className='col-6'>
                     <div className='whiteBoxTitleCareerStats'>
--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { Link, useParams } from 'react-router-dom';
 
 const Photo = () => {
+
+    let params = useParams();
+
+    const imagem1 = "https://64.media.tumblr.com/aa70522f93cb0dd091e70ff6277d7120/863d550f350bb3d5-bc/s1280x1920/c406e597f998877614a8be87333aef4b3d2ab4eb.jpg";
+    const imagem2 = "https://pbs.twimg.com/media/E9zTn2bWEAAD8W6.jpg:large";
+    const imagem3 = "https://e0.365dm.com/17/12/2048x1152/skysports-cristiano-ronaldo-real-madrid-football_4178227.jpg?20171209155617";
+    const imagem4 = "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt5648e027198a434f/64162a3012c1f4425a6f02e0/Cristiano_Ronaldo_2022-23.jpg?auto=webp&format=pjpg&width=3840&quality=60";
+
+
+    console.log("photo link: ", params.link);
+
     return (
         <>
             <NavBar />
-            <span>Photo</span>
+            
+            <div className='flexPhotos'>
+                <div className="row">
+                    <div className='col-3'>
+                        <img height={300} width={300} src={ imagem1 } />
+                    </div>
+                    <div className='col-4'>
+                        <img height={300} width={400} src={ imagem2 } />
+                    </div>
+                    <div className='col-5'>
+                        <img height={300} width={500} src={ imagem3 } />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className='col-6'>
+                        <img height={300} width={500} src={ imagem4 } />
+                    </div>
+                    <div className='col-6'>
+                        <img height={300} width={500} src={ imagem4 } />
+                    </div>
+                </div>
+            </div>
 
+        
         </>
     );
 }
