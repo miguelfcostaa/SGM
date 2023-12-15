bf8bd2627c2a46039d01935def73e728cce2eb49
xRodry <rodrigo-barradas2015@outlook.pt>
2023-12-15 15:41:00 +0000

    atualização

--- src/components/Timeline5.tsx
+++ src/components/Timeline5.tsx
@@ -13,6 +13,16 @@ const Timeline5 = () => {
                 src="https://cdn.discordapp.com/attachments/525052167007240224/1184520885105868850/ronaldo_manchester_united_2008.jpg?ex=658c460a&is=6579d10a&hm=19f43468d7c9a7b34b491f4f8d066e7677202b1d9d6314342abc0fa4befd1656&"
                 ></img>
             </Box>
+            <div>
+                <button className="invisible-button" onClick={() => window.location.href = "/timeline2"}></button>
+            </div>
+            <div className="line-full"></div>
+            <div className="ball"></div>
+            <div className="textotimeline2">
+                <p>
+                    
+                </p>
+            </div>
         </div>
     );
 }
